9bf128ea173442eea1789c0e2544ad1d729cffa6
Quang Huy <daoquanghuy2702@gmail.com>
2026-01-23 14:05:33 +0700

    update code

--- src/main/java/com/nhohantu/tcbookbe/cms/repository/ICmsCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhohantu.tcbookbe.cms.repository;
-
-import com.nhohantu.tcbookbe.common.repository.BaseCategoryRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICmsCategoryRepository extends BaseCategoryRepository {
-}
